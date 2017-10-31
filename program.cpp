2139b52ac3e7585f941d03bc09ee570dee483334
KrunoKuhtic <krunokuhtic2@gmail.com>
2017-10-31 15:35:27 +0100

    ispravak

--- program.cpp
+++ program.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int main()
@@ -23,7 +24,7 @@ int main()
             }
             else
             {
-                 cout<< "O" << "   ";
+                 cout<< "-" << "   ";
             }
 
        }
